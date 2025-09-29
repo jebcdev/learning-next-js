4aaa8478e10945584a33094b9aa967ae9a655d81
esteban5tael@gmail.com <esteban5tael@gmail.com>
2025-09-29 10:11:56 -0500

    09 Streaming Refactor dashboard components: remove unused code, implement async data fetching, and enhance loading states with skeletons.

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,10 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+
+export default function DashboardLoadingPage() {
+  return (
+    <>
+    <DashboardSkeleton/>
+    </>
+  );
+}
--- app/dashboard/customers/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function CustomersPage() {
-  return (
-    <div>
-      <h1>Customers Page</h1>
-    </div>
-  );
-}
